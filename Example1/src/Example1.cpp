8382be2de6fae39cccdaa18704c486e25328b5c9
roozbeh <roozbeh.manshaei@ryerson.ca>
2015-10-21 11:08:59 -0400

    Added a new line to the program

---
+++ Example1/src/Example1.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Example1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "My name is Roozbeh" << endl;
+	return 0;
+}
